622cbe9d5e8e0bc149f5af3627d8291ea309c5d5
Meher <mehergm@gmail.com>
2019-07-13 23:07:50 -0500

    updated create tm and search

--- client/src/components/Trademark/CreateTrademark.js
+++ client/src/components/Trademark/CreateTrademark.js
@@ -1,17 +1,11 @@
 import React, { Component } from "react";
 import styled from "styled-components";
-import {
-  Form,
-  Button,
-  Select,
-  ToastMessage,
-  Flash,
-  Textarea,
-  Icon
-} from "rimble-ui";
+import { Form, Button, Select, Textarea, Icon, Field } from "rimble-ui";
 import Dropzone from "react-dropzone";
 import ipfs from "../../utils/ipfs";
 import { withRouter } from "react-router-dom";
+import { gql } from "apollo-boost";
+import { Mutation } from "react-apollo";
 
 const MainWrapper = styled.div`
   display: flex;
@@ -79,7 +73,7 @@ class CreateTrademark extends Component {
     markResponse: false,
     propertyType: "Trademark",
     markType: "Generic",
-    className: "",
+    className: "Class 1",
     markName: "",
     markDesc: "",
     myMarks: null,
@@ -92,7 +86,6 @@ class CreateTrademark extends Component {
 
   createMark = async (markName, markDesc, markType, ipfsHash, className) => {
     const { accounts, contract } = this.props;
-    console.log(className)
     await contract.methods
       .createMark(markName, markDesc, markType, ipfsHash, className)
       .send({ from: accounts[0] });
@@ -135,157 +128,212 @@ class CreateTrademark extends Component {
     }); //await ipfs.add
   }; //onSubmit
 
-  handleSubmit = async () => {
+  handleSubmit = async CreateTrademarkMutation => {
     if (!this.state.file) {
-      throw alert("please upload a document")
+      throw alert("please upload a document");
     } else {
       await ipfs.add(this.state.buffer, async (err, ipfsHash) => {
         if (err) {
           throw err;
         }
         if (ipfsHash) {
-
-         await this.createMark(
+          await this.createMark(
             this.state.markName,
             this.state.markDesc,
             this.state.markType,
             ipfsHash[0].hash,
             this.state.className
-          )
+          );
         }
       });
-      this.props.history.push("/dashboard");
     }
   };
 
   render() {
-    console.log(this.state.className, this.state.markType)
     return (
-      <MainWrapper>
-        <Form.Field label="Type: ">
-          <Select
-            required
-            items={["Trademark", "Copyright"]}
-            onChange={e => this.setState({ propertyType: e.target.value })}
-          />
-        </Form.Field>
-        {this.state.propertyType === "Trademark" ? (
-          <>
-            <h3> Register Trademark </h3>
-            <FormStyle onSubmit={this.handleSubmit}>
-              <Form.Field label="Trademark Type: ">
+      <Mutation mutation={CreateTrademarkMutation}>
+        {(CreateTrademark, { loading, error }) => {
+          if (error) return `Error: ${error}`;
+          return (
+            <MainWrapper>
+              <Field label="Type: ">
                 <Select
                   required
-                  items={[
-                    "Generic",
-                    "Descriptive",
-                    "Suggestive",
-                    "Arbitrary/Fanciful"
-                  ]}
-                  onChange={e => this.setState({ markType: e.target.value })}
+                  items={["Trademark", "Copyright"]}
+                  onChange={e =>
+                    this.setState({ propertyType: e.target.value })
+                  }
                 />
-              </Form.Field>
-              <LineAlignmentStyle>
-                <Form.Field label="Trademark Class: ">
-                  <Select
-                    required
-                    items={[
-                      "Class 1",
-                      "Class 2",
-                      "Class 3",
-                      "Class 4",
-                      "Class 5",
-                      "Class 6",
-                      "Class 7",
-                      "Class 8",
-                      "Class 9",
-                      "Class 10",
-                      "Class 11",
-                      "Class 12",
-                      "Class 13",
-                      "Class 14",
-                      "Class 15",
-                      "Class 16",
-                      "Class 17",
-                      "Class 18",
-                      "Class 19",
-                      "Class 20",
-                      "Class 21",
-                      "Class 22",
-                      "Class 23",
-                      "Class 24",
-                      "Class 25",
-                      "Class 26",
-                      "Class 27",
-                      "Class 28",
-                      "Class 29",
-                      "Class 30",
-                      "Class 31",
-                      "Class 32",
-                      "Class 33",
-                      "Class 34",
-                      "Class 35",
-                      "Class 36",
-                      "Class 37",
-                      "Class 38",
-                      "Class 39",
-                      "Class 40",
-                      "Class 41",
-                      "Class 42",
-                      "Class 43",
-                      "Class 44",
-                      "Class 45"
-                    ]}
-                    onChange={e => this.setState({ className: e.target.value })}
-                  />
-                </Form.Field>
-                <a href="/resources/trademark-class-list" target="_blank">
-                  (see class list)
-                </a>
-              </LineAlignmentStyle>
-              <Form.Field label="Trademark Name: ">
-                <Form.Input
-                  width={520}
-                  required
-                  onChange={e => this.setState({ markName: e.target.value })}
-                />
-              </Form.Field>
-              <Form.Field label="Trademark Description: ">
-                <Textarea
-                  width={520}
-                  rows={3}
-                  required
-                  onChange={e => this.setState({ markDesc: e.target.value })}
-                />
-              </Form.Field>
-              <FieldLabel>Upload document to prove prior use: </FieldLabel>
-              <Form.Field label="Upload document to prove prior use: ">
-                <Dropzone onDrop={this.onDrop}>
-                  {({ getRootProps, getInputProps }) => (
-                    <DropZoneStyle {...getRootProps()}>
-                      <Icon color="primary" name="CloudUpload" />
-                      <input {...getInputProps()} />
-                      {!this.state.file ? (
-                        <p>Drag 'n' drop file here, or click to select</p>
-                      ) : (
-                          <p>{this.state.file && this.state.file.name}</p>
+              </Field>
+              {this.state.propertyType === "Trademark" ? (
+                <>
+                  <h3> Register Trademark </h3>
+
+                  <FormStyle>
+                    <Field label="Trademark Type: ">
+                      <Select
+                        items={[
+                          "Generic",
+                          "Descriptive",
+                          "Suggestive",
+                          "Arbitrary/Fanciful"
+                        ]}
+                        onChange={e =>
+                          this.setState({ markType: e.target.value })
+                        }
+                      />
+                    </Field>
+                    <LineAlignmentStyle>
+                      <Field label="Trademark Class: ">
+                        <Select
+                          items={[
+                            "Class 1",
+                            "Class 2",
+                            "Class 3",
+                            "Class 4",
+                            "Class 5",
+                            "Class 6",
+                            "Class 7",
+                            "Class 8",
+                            "Class 9",
+                            "Class 10",
+                            "Class 11",
+                            "Class 12",
+                            "Class 13",
+                            "Class 14",
+                            "Class 15",
+                            "Class 16",
+                            "Class 17",
+                            "Class 18",
+                            "Class 19",
+                            "Class 20",
+                            "Class 21",
+                            "Class 22",
+                            "Class 23",
+                            "Class 24",
+                            "Class 25",
+                            "Class 26",
+                            "Class 27",
+                            "Class 28",
+                            "Class 29",
+                            "Class 30",
+                            "Class 31",
+                            "Class 32",
+                            "Class 33",
+                            "Class 34",
+                            "Class 35",
+                            "Class 36",
+                            "Class 37",
+                            "Class 38",
+                            "Class 39",
+                            "Class 40",
+                            "Class 41",
+                            "Class 42",
+                            "Class 43",
+                            "Class 44",
+                            "Class 45"
+                          ]}
+                          onChange={e =>
+                            this.setState({ className: e.target.value })
+                          }
+                        />
+                      </Field>
+                      <a href="/resources/trademark-class-list" target="_blank">
+                        (see class list)
+                      </a>
+                    </LineAlignmentStyle>
+                    <Field label="Trademark Name: ">
+                      <Form.Input
+                        width={520}
+                        onChange={e =>
+                          this.setState({ markName: e.target.value })
+                        }
+                      />
+                    </Field>
+                    <Field label="Trademark Description: ">
+                      <Textarea
+                        width={520}
+                        rows={3}
+                        onChange={e =>
+                          this.setState({ markDesc: e.target.value })
+                        }
+                      />
+                    </Field>
+                    <Field label="Upload document to prove prior use: ">
+                      <Dropzone onDrop={this.onDrop}>
+                        {({ getRootProps, getInputProps }) => (
+                          <DropZoneStyle {...getRootProps()}>
+                            <Icon color="primary" name="CloudUpload" />
+                            <input {...getInputProps()} />
+                            {!this.state.file ? (
+                              <p>Drag 'n' drop file here, or click to select</p>
+                            ) : (
+                              <p>{this.state.file && this.state.file.name}</p>
+                            )}
+                          </DropZoneStyle>
                         )}
-                    </DropZoneStyle>
-                  )}
-                </Dropzone>
-              </Form.Field>
-              <Button type="submit">Register</Button>
-            </FormStyle>
-          </>
-        ) : (
-            <>
-              <h3>Register Copyright</h3>
-              <p>Coming soon...!</p>
-            </>
-          )}
-      </MainWrapper>
+                      </Dropzone>
+                    </Field>
+                    <Button
+                      disabled={
+                        loading ||
+                        !this.state.markName ||
+                        !this.state.markDesc ||
+                        !this.state.buffer
+                      }
+                      onClick={async e => {
+                        e.preventDefault();
+                        e.stopPropagation();
+                        console.log(this.state.className);
+                        await CreateTrademark({
+                          variables: {
+                            name: this.state.markName,
+                            description: this.state.markDesc,
+                            type: this.state.markType,
+                            className: this.state.className
+                          }
+                        });
+                        await this.handleSubmit();
+                        await setTimeout(function() {
+                          return;
+                        }, 500);
+                        this.props.history.push("/dashboard");
+                      }}
+                    >
+                      Register
+                    </Button>
+                  </FormStyle>
+                </>
+              ) : (
+                <>
+                  <h3>Register Copyright</h3>
+                  <p>Coming soon...!</p>
+                </>
+              )}
+            </MainWrapper>
+          );
+        }}
+      </Mutation>
     );
   }
 }
 
 export default withRouter(CreateTrademark);
+
+const CreateTrademarkMutation = gql`
+  mutation CreateTrademarkMutation(
+    $name: String!
+    $description: String!
+    $type: String!
+    $className: String!
+  ) {
+    createTrademark(
+      name: $name
+      description: $description
+      type: $type
+      className: $className
+    ) {
+      id
+    }
+  }
+`;
